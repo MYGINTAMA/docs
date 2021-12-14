158c34cba9a397b128d744786db5c6a679a4e55a
tomy <zhu_mei_yu@163.com>
2021-12-14 10:57:40 +0800

    up

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+yarn run docs:build
+
+# 进入生成的文件夹
+cd docs/.vitepress/dist
+
+git init
+git add -A
+git commit -m 'deploy to the gh-pages'
+
+# 如果发布到 https://<USERNAME>.github.io
+git push -f git@github.com:MYGINTAMA/docs.git master:gh-pages
